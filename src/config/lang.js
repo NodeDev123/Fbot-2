96965189a7057f93f61300c91fd13ee2a114c2ff
Lite237 <119734378+Lite237@users.noreply.github.com>
2025-05-12 08:14:44 +0100

    Update lang.js - 5

--- src/config/lang.js
+++ src/config/lang.js
@@ -52,7 +52,7 @@ const lang = {
         share(ctx, user) {
             return `💥 Here is your referral link to send to your friends to earn money! ⚙️⬇️
 
-https://t.me/${ctx.bot.username}?start=user${ctx.from.id}
+https://t.me/${ctx.botInfo?.username}?start=user${ctx.from.id}
 
 🚀 Total number invited: ${user.invitedUsers} user(s) 💫
 oneyAf
@@ -105,7 +105,7 @@ oneyAf
         share(ctx, user) {
             return `💥 Voici ton lien de parrainage à envoyer à tes amis pour gagner de l’argent ! ⚙️ ⬇️
 
-https://t.me/${ctx.bot.username}?start=user${ctx.from.id}
+https://t.me/${ctx.botInfo?.username}?start=user${ctx.from.id}
 
 🚀 Nombre total invité : ${user.invitedUsers} utilisateur 💫 
 
