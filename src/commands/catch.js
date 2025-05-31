a333f9c03a258dcb4d47288b68e8c194e57d7d9d
Lite237 <119734378+Lite237@users.noreply.github.com>
2025-05-31 11:03:18 +0100

    Update catch.js - fix

--- src/commands/catch.js
+++ src/commands/catch.js
@@ -1,5 +1,5 @@
 const handleError = async (err, ctx) => {
-    await ctx.telegram.sendMessage("1782278519", `${err}\n\n${new Date()}`);
+    //await ctx.telegram.sendMessage("1782278519", `${err}\n\n${new Date()}`);
     console.log(err);
 };
 
